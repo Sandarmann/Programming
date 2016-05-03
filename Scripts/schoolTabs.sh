198364112c23d3df428cfd83ead806c81993d107
Aleksandar <alstraumann@gmail.com>
2016-05-03 11:22:53 -0500

    touch up

--- Scripts/schoolTabs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-open -a "Google Chrome" 'https://google.com' 'https://news.ycombinator.com/news' 'https://sivelab.slack.com/' 'https://www.reddit.com/r/programming/' 'http://stackoverflow.com/'
